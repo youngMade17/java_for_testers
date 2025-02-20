d914a756cd5a340474557dd4e14752669fb8fef7
Mikhail Gundenkov_idea <gundenkov.99@gmail.com>
2025-02-21 00:53:47 +0300

    Лекция 5.8. Описание конфигурации тестового стенда

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
@@ -7,13 +7,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
     protected WebDriver driver;
     private static LoginHelper session;
-    public static GroupHelper groups;
-    public static ContactHelper contacts;
+    private static GroupHelper groups;
+    private static ContactHelper contacts;
+    private static Properties properties;
 
-    public void init(String browser) {
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if ("edge".equals(browser)) {
                 driver = new EdgeDriver();
@@ -23,9 +27,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser: %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(1936, 1048));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -1,21 +1,26 @@
 package ru.stqa.addressbook.tests;
 
-import ru.stqa.addressbook.manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
+import ru.stqa.addressbook.manager.ApplicationManager;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.Random;
 
 public class TestBase {
     protected static ApplicationManager app;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         if (app == null) {
             app = new ApplicationManager();
         }
-        app.init(System.getProperty("browser", "chrome"));
+        var properties = new Properties();
+        properties.load(new FileReader(System.getProperty("target", "local.properties")));
+        app.init(System.getProperty("browser", "chrome"), properties);
     }
 
     public static String randomFile(String dir) {
