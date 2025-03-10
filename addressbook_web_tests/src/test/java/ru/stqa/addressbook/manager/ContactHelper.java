405a7edbbc36a0b69390b7497dee5a66c9fb774d
Mikhail Gundenkov_idea <gundenkov.99@gmail.com>
2025-03-10 22:20:03 +0300

    Лекция 7.6. Словари, или ассоциативные массивы (Map)

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ContactHelper.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ContactHelper extends HelperBase {
 
@@ -191,5 +193,16 @@ public class ContactHelper extends HelperBase {
 
     public String getPhones(ContactData contact) {
         return manager.driver.findElement(By.xpath(String.format("//input[@id='%s']/../../td[6]", contact.id()))).getText();
+    }
+
+    public Map<String, String> getPhones() {
+        var result = new HashMap<String, String>();
+        List<WebElement> rows = manager.driver.findElements(By.name("entry"));
+        for (WebElement row : rows) {
+            var id = row.findElement(By.tagName("input")).getAttribute("id");
+            var phones = row.findElements(By.tagName("td")).get(5).getText();
+            result.put(id, phones);
+        }
+        return result;
     }
 }
--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactInfoTests.java
@@ -2,6 +2,7 @@ package ru.stqa.addressbook.tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.stqa.addressbook.model.ContactData;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,11 +11,21 @@ public class ContactInfoTests extends TestBase {
     @Test
     void testPhones() {
         var contacts = app.hbm().getContactList();
-        var contact = contacts.get(0);
-        var phones = app.contacts().getPhones(contact);
-        var expected = Stream.of(contact.home(), contact.mobile(), contact.work())
-                .filter(s -> s != null && !"".equals(s))
-                .collect(Collectors.joining("\n"));
+        var expected = contacts.stream().collect(Collectors.toMap(
+                ContactData::id,
+                contact -> Stream.of(contact.home(), contact.mobile(), contact.work())
+                        .filter(s -> s != null && !"".equals(s))
+                        .collect(Collectors.joining("\n"))
+        ));
+        var phones = app.contacts().getPhones();
         Assertions.assertEquals(expected, phones);
+
+//        for (var contact : contacts) {
+//            //var phones = app.contacts().getPhones(contact);
+//            var expected = Stream.of(contact.home(), contact.mobile(), contact.work())
+//                    .filter(s -> s != null && !"".equals(s))
+//                    .collect(Collectors.joining("\n"));
+//            Assertions.assertEquals(expected, phones.get(contact.id()));
+//        }
     }
 }
